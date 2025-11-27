58cb554dab60514aea9708c75b9311d27b00d8ce
Kamil <kamil.gwozdz9@interia.pl>
2025-11-27 22:46:51 +0100

    fix: clean up terraform documentation

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,20 +1,15 @@
-# terraform/providers.tf
-
-# Określamy wymaganą wersję Terraform
 terraform {
-  required_version = ">= 1.0.0"  # Minimum wersja 1.0.0
+  required_version = ">= 1.0.0"
   
-  # Lista providerów których używamy
   required_providers {
     google = {
-      source  = "hashicorp/google"  # Skąd pobrać provider
-      version = "~> 5.0"             # Wersja 5.x (najnowsza stabilna)
+      source  = "hashicorp/google"
+      version = "~> 5.0"
     }
   }
 }
 
-# Konfiguracja providera Google
 provider "google" {
-  project = var.project_id   # ID projektu GCP (z variables.tf)
-  region  = var.region       # Region (np. europe-central2 = Warszawa)
+  project = var.project_id
+  region  = var.region
 }
