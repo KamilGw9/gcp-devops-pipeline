07d9b39bce75af6e28f401a0767e9e55fcf2495d
Kamil <kamil.gwozdz9@interia.pl>
2025-12-11 22:49:53 +0100

    feat: Cfg terraform remote backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.0.0"
   
+  backend "gcs" {
+    bucket = "gcp-devops-pipelien-kamil-tfstate"
+    prefix = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
